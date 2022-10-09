85ff182c9359d9b63c88adf4411ebf6220ea8459
teaforbreakfast <114776467+teaforbreakfast@users.noreply.github.com>
2022-10-09 16:34:32 -0500

    Add files via upload

---
+++ My Jupyter Workbook on IBM Watson Studio.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Workbook on IBM Watson Studio
+
+# **Stella Yan** 
+# Data Scientist
+
+# *I am interested in data science because I am an analytic person and would love to apply my analytic abilities in data science.*
+
+# ### My code estimates how much a person would have if the inidividual invested 1,000 dollars in the S&P 500 for 10 years (assuming a 10% return and dividends reinvested).
+
+# In[3]:
+
+
+1000*1.1**10
+
+
+# Numbered List: 
+# 1. one
+# 2. two
+# 3. three
+# 
+# Bulleted List:
+# * A
+# * B
+# * C
+# 
+# 
+# ***
+# Horizontal Lines
+# ***
+# 
+# 
+# 
+# 
+# 
